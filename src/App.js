2ee1956562c19fc72a271cbfb6a396668000b78d
meriem <elounimeriem@gmail.com>
2022-02-02 15:40:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Navbar, Container, Nav} from 'react-bootstrap'
 import './App.css';
+import pictureNature from './arbre.jpeg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand className='nav' href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        </Container>
+      </Navbar>
+      <h1 className='title'> Ws REACT JS </h1>
+      <p style={{color:'pink', marginTop:'100px'}} >this is a paragraph</p>
+      <img src='/public/mer.jpeg' alt='pict' />
+      <img src={pictureNature} alt='pic' />
     </div>
   );
 }
